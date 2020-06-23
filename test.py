66ffe82b6b2d7ccee12f5163a90db092702cd1bb
sremedios <sam.remedios@gmail.com>
2020-06-23 11:48:06 -0400

    sdf

--- test.py
+++ test.py
@@ -114,7 +114,7 @@ if __name__ == '__main__':
                 fontsize=14,
             )
 
-    print("Balanced accuracy score: {:.4f}".format(bas))
+    print("Balanced accuracy score: {:.4%}".format(bas))
             
     fig.tight_layout()
     plt.title("Balanced accuracy score: {:.2%}".format(bas))
